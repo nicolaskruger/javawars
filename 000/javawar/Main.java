2b082848cd8a885343c9cfe45cda42abba06735c
ni <nicolas.nkruger@gmail.com>
2025-10-02 06:37:16 -0300

    save

--- 000/javawar/Main.java
+++ 000/javawar/Main.java
@@ -1,5 +1,21 @@
 
 public class Main {
+  public static String _binaryAddition(int a, int b) {
+    // your code here
+    return Integer.toBinaryString(a + b);
+  }
+
+  public static String bit(int num) {
+    boolean one = (num % 2 == 1);
+    if (num / 2 == 0)
+      return one ? "1" : "";
+    return bit(num / 2) + (one ? "1" : "0");
+  }
+
+  public static String binaryAddition(int a, int b) {
+    return bit(a + b);
+  }
+
   public static void main(String[] args) {
     System.out.println("hello");
   }
